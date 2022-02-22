b8ac1142af9b0c1f9f231d4765323c1a91dd17aa
Samuel Larkin <Samuel.Larkin@cnrc-nrc.gc.ca>
2022-02-22 10:37:46 -0500

    Added unittests.

--- src/main.rs
+++ src/main.rs
@@ -60,7 +60,37 @@ fn l<R, I, T>(iter: &mut I, size: usize, rng: &mut R) -> Vec<T>
 
 fn main() {
     let mut rng = thread_rng();
-    println!("Hello, world!");
-    println!("{:?}", fill(&mut(0usize..100), 7));
-    println!("{:?}", l(&mut(0usize..100), 7, &mut rng));
+}
+
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn fill_short() {
+        let result = fill(&mut(0usize..5), 7);
+        assert_eq!(result, (0usize..5).into_iter().collect::<Vec<_>>());
+    }
+
+    #[test]
+    fn fill_long() {
+        let result = fill(&mut(0usize..100), 7);
+        assert_eq!(result, (0usize..7).into_iter().collect::<Vec<_>>());
+    }
+
+    #[test]
+    fn l_short() {
+        let mut rng = thread_rng();
+        let result = l(&mut(0usize..5), 7, &mut rng);
+        assert_eq!(result, (0usize..5).into_iter().collect::<Vec<_>>());
+    }
+
+    #[test]
+    fn l_long() {
+        let mut rng = thread_rng();
+        let result = l(&mut(-100isize..100), 7, &mut rng);
+        assert!(result.iter().all(|&v| -100 <= v && v < 100));
+    }
 }
