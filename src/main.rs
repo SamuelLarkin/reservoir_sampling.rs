d6073f4d2124e876e54e91f04ea436afadb10b3f
Samuel Larkin <Samuel.Larkin@cnrc-nrc.gc.ca>
2022-03-07 14:38:25 -0500

    Added a subcommand for a_exp_j().

--- src/main.rs
+++ src/main.rs
@@ -6,35 +6,105 @@
  * cargo test -- --nocapture
  */
 
-use clap::Parser;
+use clap::{
+    Parser,
+    Subcommand,
+};
 use rand::thread_rng;
 use std::io::{
     stdin,
-    BufRead,
 };
-use reservoir_sampling::l;
+use reservoir_sampling::{
+    a_exp_j,
+    l,
+};
 
 
 
-#[derive(Parser, Debug)]
-#[clap(author, version, about, long_about = None)]
-struct Args {
-    #[clap(short, long, default_value_t = 10)]
-    size: usize,
+#[derive(Parser)]
+#[clap(name = "reservoir_sampling")]
+#[clap(author, version, about="Sampling a stream", long_about = None)]
+struct Cli {
+    #[clap(subcommand)]
+    command: Commands,
 }
 
 
 
+#[derive(Subcommand)]
+enum Commands {
+    #[clap(arg_required_else_help = false)]
+    Unweighted {
+        #[clap(short, long, default_value_t = 10)]
+        size: usize,
+    },
+
+    #[clap(arg_required_else_help = false)]
+    Weighted {
+        #[clap(short, long, default_value_t = 10)]
+        size: usize,
+
+        #[clap(name="weights' file name")]
+        weight_fn: String,
+
+        #[clap(name="values' file name")]
+        value_fn: String,
+    },
+}
+
+
+use std::fs::File;
+use std::io::{
+    self,
+    BufRead,
+};
+use std::path::Path;
+// The output is wrapped in a Result to allow matching on errors
+// Returns an Iterator to the Reader of the lines of the file.
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+    where
+        P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
+
+
+
+
+
 fn main() {
-    let args = Args::parse();
-    let input = stdin();
-    let mut input_lines = input.lock().lines().map(|r| r.unwrap());
-    let mut rng = thread_rng();
-    let samples = l(
-        &mut input_lines,
-        args.size,
-        &mut rng);
-    for sample in samples {
-        println!("{}", sample);
-    }
+    let args = Cli::parse();
+    match &args.command {
+        Commands::Unweighted { size } => {
+            let mut rng = thread_rng();
+            let input = stdin();
+            let mut input_lines = input.lock().lines().map(|r| r.unwrap());
+            let samples = l(
+                &mut input_lines,
+                *size,
+                &mut rng);
+            for sample in samples {
+                println!("{}", sample);
+            }
+        }
+        Commands::Weighted { size, weight_fn, value_fn } => {
+            let mut rng = thread_rng();
+            let weights = read_lines(weight_fn)
+                .unwrap()
+                .map(Result::unwrap)
+                .map(|v| v.parse::<f64>().unwrap());
+            let values = read_lines(value_fn)
+                .unwrap()
+                .map(Result::unwrap);
+            let samples = a_exp_j(
+                &mut weights.zip(values),
+                *size,
+                &mut rng);
+            for sample in samples {
+                println!("{}", sample);
+            }
+        }
+    };
 }
