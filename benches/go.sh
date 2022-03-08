2d2c395924f0e0c9787d687c746731963ad7a3a5
Samuel Larkin <Samuel.Larkin@cnrc-nrc.gc.ca>
2022-03-08 14:25:30 -0500

    Using prepare/cleanup.

--- benches/go.sh
+++ benches/go.sh
@@ -3,12 +3,14 @@
 
 readonly python="~/git/reservoir_sampling/reservoir_sampling/reservoir_sampling.py"
 readonly sample_size=1000
+readonly max_stream_size=500000
 readonly src=${1:-$PORTAGE/corpora/bac-lac.2021/bitextor_2018/201808/permanent/en-fr.deduped.txt.gz}
 
 cargo build --release
-zcat --force $src | head -500000 > benches/src.txt
 hyperfine \
    --shell bash \
+   --prepare "zcat --force $src | head -n $max_stream_size > benches/src.txt" \
+   --cleanup "rm benches/src.txt" \
    --export-json hyperfine.text.json \
    --style full \
    --parameter-list sample_size 1000,10000,100000 \
@@ -18,4 +20,3 @@ hyperfine \
    "cargo run --release weighted --size {sample_size} benches/src.txt <(cut -f 8 < benches/src.txt)" \
    | tee \
    > hyperfine.text.json.results
-rm benches/src.txt
