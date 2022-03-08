955c489c970c215231b563e338f45dee273d43f0
Samuel Larkin <Samuel.Larkin@cnrc-nrc.gc.ca>
2022-03-08 14:26:56 -0500

    Using multiple sample sizes.

--- benches/go.sh
+++ benches/go.sh
@@ -2,7 +2,7 @@
 
 
 readonly python="~/git/reservoir_sampling/reservoir_sampling/reservoir_sampling.py"
-readonly sample_size=1000
+readonly sample_sizes=1000,5000,10000
 readonly max_stream_size=500000
 readonly src=${1:-$PORTAGE/corpora/bac-lac.2021/bitextor_2018/201808/permanent/en-fr.deduped.txt.gz}
 
@@ -13,10 +13,10 @@ hyperfine \
    --cleanup "rm benches/src.txt" \
    --export-json hyperfine.text.json \
    --style full \
-   --parameter-list sample_size 1000,10000,100000 \
-   "$python --size {sample_size} < benches/src.txt" \
+   --parameter-list sample_size $sample_sizes \
+   "$python unweighted --size {sample_size} benches/src.txt" \
    "cargo run --release unweighted --size {sample_size} < benches/src.txt" \
-   "$python --size {sample_size} benches/src.txt <(cut -f 8 < benches/src.txt)" \
+   "$python weighted --size {sample_size} benches/src.txt <(cut -f 8 < benches/src.txt)" \
    "cargo run --release weighted --size {sample_size} benches/src.txt <(cut -f 8 < benches/src.txt)" \
    | tee \
    > hyperfine.text.json.results
