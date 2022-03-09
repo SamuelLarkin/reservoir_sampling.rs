abfb3bd94fa5de7580e6de54ca038c44561cede6
Samuel Larkin <Samuel.Larkin@cnrc-nrc.gc.ca>
2022-03-09 10:08:49 -0500

    Revisiting the CLI options/arguments.

--- src/main.rs
+++ src/main.rs
@@ -10,12 +10,16 @@ use clap::{
     Parser,
     Subcommand,
 };
-use rand::thread_rng;
+use rand::{
+    SeedableRng,
+    rngs::SmallRng,
+    thread_rng,
+};
 use std::fs::File;
 use std::io::{
     BufRead,
+    BufReader,
     self,
-    stdin,
 };
 use std::path::Path;
 use reservoir_sampling::{
@@ -37,20 +41,37 @@ struct Cli {
 
 #[derive(Subcommand)]
 enum Commands {
-    #[clap(arg_required_else_help = false)]
+    #[clap(arg_required_else_help=false)]
+    /// Unweighted resevoir sampling
     Unweighted {
-        #[clap(short, long, default_value_t = 10)]
+        /// Seed for reproducibility
+        #[clap(short, long, parse(try_from_str))]
+        seed: Option<u64>,
+
+        /// Sample size
+        #[clap(short, long, default_value_t=10)]
         size: usize,
+
+        /// Population file name.
+        #[clap(name="population's file name")]
+        population_fn: Option<String>,
     },
 
     #[clap(arg_required_else_help = false)]
     Weighted {
-        #[clap(short, long, default_value_t = 10)]
+        /// Seed for reproducibility
+        #[clap(short, long, parse(try_from_str))]
+        seed: Option<u64>,
+
+        /// Sample size
+        #[clap(short, long, default_value_t=10)]
         size: usize,
 
-        #[clap(name="values' file name")]
-        value_fn: String,
+        /// Population file name.
+        #[clap(name="population's file name")]
+        population_fn: String,
 
+        /// Weights file name.
         #[clap(name="weights' file name")]
         weight_fn: String,
     },
@@ -60,14 +81,35 @@ enum Commands {
 
 // The output is wrapped in a Result to allow matching on errors
 // Returns an Iterator to the Reader of the lines of the file.
-fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<BufReader<File>>>
     where
         P: AsRef<Path>,
 {
     let file = File::open(filename)?;
-    Ok(io::BufReader::new(file).lines())
+    Ok(BufReader::new(file).lines())
 }
 
+
+
+/// Return an open file or stdin if no filename.
+fn get_reader(filename: &Option<String>) -> Box<dyn BufRead>
+{
+    match filename {
+        None => Box::new(BufReader::new(io::stdin())),
+        Some(filename) => Box::new(BufReader::new(File::open(filename).unwrap()))
+    }
+}
+
+
+
+/// Use the seed to seed a SmallRng or else use thread_rng().
+fn get_rng(seed: &Option<u64>) -> SmallRng
+{
+    match seed {
+        None => SmallRng::from_rng(thread_rng()).unwrap(),
+        Some(s) => SmallRng::seed_from_u64(*s)
+    }
+}
 
 
 
@@ -76,29 +118,33 @@ fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
 fn main() {
     let args = Cli::parse();
     match &args.command {
-        Commands::Unweighted { size } => {
-            let mut rng = thread_rng();
-            let input = stdin();
-            let mut input_lines = input.lock().lines().map(|r| r.unwrap());
+        Commands::Unweighted { size, seed, population_fn } => {
+            let mut rng = get_rng(seed);
+            // https://stackoverflow.com/a/49964042
+            // https://www.reddit.com/r/rust/comments/jv3q3e/comment/gci1mww/?utm_source=share&utm_medium=web2x&context=3
+            let population = get_reader(population_fn);
+
             let samples = l(
-                &mut input_lines,
+                &mut population.lines().map(|v| v.unwrap()),
                 *size,
                 &mut rng);
             for sample in samples {
                 println!("{}", sample);
             }
         }
-        Commands::Weighted { size, weight_fn, value_fn } => {
-            let mut rng = thread_rng();
+        Commands::Weighted { size, seed, weight_fn, population_fn } => {
+            let mut rng = get_rng(seed);
             let weights = read_lines(weight_fn)
                 .unwrap()
                 .map(Result::unwrap)
                 .map(|v| v.parse::<f64>().unwrap());
-            let values = read_lines(value_fn)
+            let values = read_lines(population_fn)
                 .unwrap()
                 .map(Result::unwrap);
+            let mut weighted_samples = weights.zip(values);
+
             let samples = a_exp_j(
-                &mut weights.zip(values),
+                &mut weighted_samples,
                 *size,
                 &mut rng);
             for sample in samples {
