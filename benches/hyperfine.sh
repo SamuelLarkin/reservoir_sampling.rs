8673916ee04b782bbc845da4920f412a4e001e37
Samuel Larkin <Samuel.Larkin@cnrc-nrc.gc.ca>
2022-03-09 10:37:02 -0500

    BAKA don't change your script while running while also pushing the changes thinking everything is running fine :(

--- benches/hyperfine.sh
+++ benches/hyperfine.sh
@@ -16,9 +16,9 @@ hyperfine \
    --export-json hyperfine.text.json \
    --style full \
    --parameter-list sample_size $sample_sizes \
-   "$python unweighted --size {sample_size} benches/src.txt" \
-   "cargo run --release unweighted --size {sample_size} < benches/src.txt" \
-   "$python weighted --size {sample_size} benches/src.txt <(cut -f 8 < benches/src.txt)" \
-   "cargo run --release weighted --size {sample_size} benches/src.txt <(cut -f 8 < benches/src.txt)" \
+   "$python unweighted --size {sample_size} $tmp_src" \
+   "cargo run --release unweighted --size {sample_size} < $tmp_src" \
+   "$python weighted --size {sample_size} $tmp_src <(cut -f 8 < $tmp_src)" \
+   "cargo run --release weighted --size {sample_size} $tmp_src <(cut -f 8 < $tmp_src)" \
    | tee \
    > hyperfine.text.json.results
