9ac700df14d88c516077a6fcd79921fcbbdab6c4
Samuel Larkin <Samuel.Larkin@cnrc-nrc.gc.ca>
2022-03-09 10:22:38 -0500

    Create a temp file under /tmp for stability.

--- benches/hyperfine.sh
+++ benches/hyperfine.sh
@@ -5,12 +5,14 @@ readonly python="reservoir_sampling"
 readonly sample_sizes=${1:-1000,5000,10000}
 readonly max_stream_size=${2:-500000}
 readonly src=${3:-$PORTAGE/corpora/bac-lac.2021/bitextor_2018/201808/permanent/en-fr.deduped.txt.gz}
+readonly tmp_src=`mktemp "/tmp/reservoir_sampling.src.XXXXX"`
+
 
 cargo build --release
 hyperfine \
    --shell bash \
-   --prepare "zcat --force $src | head -n $max_stream_size > benches/src.txt" \
-   --cleanup "rm benches/src.txt" \
+   --prepare "zcat --force $src | head -n $max_stream_size > $tmp_src" \
+   --cleanup "rm $tmp_src" \
    --export-json hyperfine.text.json \
    --style full \
    --parameter-list sample_size $sample_sizes \
